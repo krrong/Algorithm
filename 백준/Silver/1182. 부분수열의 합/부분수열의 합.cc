0c900185e1b8554d00318db76c37deaeecd4db56
krrong <khk9664@naver.com>
2023-09-25 20:59:56 +0900

    [Silver II] Title: 부분수열의 합, Time: 52 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,48 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-// 합계가 S인 경우 ans++ 해줌
-// 합계가 S보다 커도 계속 확인해야 함 -> 음수를 원할 수도 있기 때문에
+int n, s, res, sum;
+bool visit[21];
 
-int N, S, ans = 0;
-vector<int> numbers;
-bool visited[21];
+void func(int k, vector<int> v, int idx) {
 
-// n번 째 수까지의 합이 sum이다.
-void search(int n, int sum) {
-    // 합계가 S인 경우 ans++
-    if(n > 0 && sum == S) {
-        ans++;
-    }
+	if (k > 0 && sum == s) {
+		res++;
+	}
 
-    for(int i = n; i < N; i++) {
-        // 이미 선택한 수라면 패스
-        if(visited[i] == true) continue;
-
-        visited[i] = true;
-        sum = sum + numbers[i];
-        search(i + 1, sum);
-        sum = sum - numbers[i];
-        visited[i] = false;
-    }
+	for (int i = idx; i < n; i++) {
+		if (visit[i] == false) {
+			visit[i] = true;
+			sum = sum + v[i];
+			func(k + 1, v, i + 1);
+			visit[i] = false;
+			sum = sum - v[i];
+		}
+	}
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    cin >> N >> S;
-    for(int i = 0; i < N; i++) {
-        int tmp;
-        cin >> tmp;
-        numbers.push_back(tmp);
-    }
+	cin >> n >> s;
 
-    search(0, 0);
-    cout << ans;
+	vector<int> v(n);
 
-    return 0;
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+
+	func(0, v, 0);
+	cout << res;
+
+	return 0;
 }
-
